fbb2dbc28f0a25cd98a9480bf15f95f5601afe44
Evgeniya Korchagina <evgeniya.korchagina@nbc.ca>
2020-04-22 00:50:15 -0400

    boostrap styles work fine

---
+++ app/Http/Controllers/AdminUsersController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UsersEditRequest;
+use App\Http\Requests\UsersRequest;
+use Illuminate\Http\Request;
+use App\User;
+use App\Role;
+use App\Photo;
+
+class AdminUsersController extends Controller
+{
+
+    public function index()
+    {
+        $users = User::all();
+        return view('admin.users.index', compact('users'));
+    }
+
+//    public function create()
+//    {
+//
+//        $roles = Role::pluck('name', 'id')->all();
+//
+//        return view('admin.users.create', compact('roles'));
+//    }
+//
+//    public function show($id)
+//    {
+//        return view('admin.users.show');
+//    }
+//
+//    public function store(UsersRequest $request)
+//    {
+//
+//        if(trim($request->password == '')){
+//            $input = $request->except('password');
+//        } else {
+//            $input = $request->all();
+//            $input['password'] = bcrypt($request->password);
+//        };
+//
+//        if ($file = $request->file('photo_id')) {
+//
+//            $name = time() . $file->getClientOriginalName();
+//            $file->move('images', $name);
+//
+//            $photo = Photo::create(['file' => $name]);
+//
+//            $input['photo_id'] = $photo->id;
+//        }
+//
+//        User::create($input);
+//
+//        return redirect('/admin/users');
+//    }
+//
+//
+//    /**
+//     * Show the form for editing the specified resource.
+//     *
+//     * @param  int $id
+//     * @return \Illuminate\Http\Response
+//     */
+//    public function edit($id)
+//    {
+//        //
+//        $user = User::findOrFail($id);
+//
+//        $roles = Role::pluck('name', 'id')->all();
+//
+//        return view('admin.users.edit', compact('user','roles'));
+//
+//    }
+//
+//    /**
+//     * Update the specified resource in storage.
+//     *
+//     * @param  \Illuminate\Http\Request $request
+//     * @param  int $id
+//     * @return \Illuminate\Http\Response
+//     */
+//    public function update(UsersEditRequest $request, $id)
+//    {
+//        //
+//
+//        $user = User::findOrFail($id);
+//
+//        if(trim($request->password == '')){
+//            $input = $request->except('password');
+//        } else {
+//            $input = $request->all();
+//            $input['password'] = bcrypt($request->password);
+//        };
+//
+////        unset($input['password']);
+//
+//        if($file = $request->file('photo_id')) {
+//            $name = time(). $file->getClientOriginalName();
+//            $file->move('images', $name);
+//            $photo = Photo::create(['file'=>$name]);
+//
+//            $input['photo_id'] = $photo->id;
+//        };
+//
+//        $user->update($input);
+//
+//        return redirect('admin/users');
+//    }
+//
+//    /**
+//     * Remove the specified resource from storage.
+//     *
+//     * @param  int $id
+//     * @return \Illuminate\Http\Response
+//     */
+//    public function destroy($id)
+//    {
+//        //
+//    }
+}
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,44 +1,45 @@
-@extends ('layouts.admin')
-
-
-@section('content')
-
-<h1> Users </h1>
-
-<table class="table table-hover">
-    <thead>
-    <tr>
-        <th>Id</th>
-        <th>Photo</th>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Role</th>
-        <th>Status</th>
-        <th>Created</th>
-        <th>Updated</th>
-    </tr>
-    </thead>
-    <tbody>
-
-    @if($users)
-
-    @foreach($users as $user)
-
-    <tr>
-        <td>{{$user->id}}</td>
-        <td><img height = "50" src="{{$user->photo? $user->photo->file:'http://placehold.it/400x400'}}" alt=""></td>
-        <td><a href="{{route('users.edit', $user->id)}}">{{$user->name}}</a></td>
-        <td>{{$user->email}}</td>
-        <td>{{$user->role->name}}</td>
-        <td>{{$user->is_active ? 'Active' : 'Inactive'}}</td>
-        <td>{{$user->created_at->diffForHumans()}}</td>
-        <td>{{$user->updated_at->diffForHumans()}}</td>
-    </tr>
-
-    @endforeach
-
-    @endif
-    </tbody>
-</table>
-
-@stop
+<h1>It is working</h1>
+<!--@extends ('layouts.admin')-->
+<!---->
+<!---->
+<!--@section('content')-->
+<!---->
+<!--<h1> Users </h1>-->
+<!---->
+<!--<table class="table table-hover">-->
+<!--    <thead>-->
+<!--    <tr>-->
+<!--        <th>Id</th>-->
+<!--        <th>Photo</th>-->
+<!--        <th>Name</th>-->
+<!--        <th>Email</th>-->
+<!--        <th>Role</th>-->
+<!--        <th>Status</th>-->
+<!--        <th>Created</th>-->
+<!--        <th>Updated</th>-->
+<!--    </tr>-->
+<!--    </thead>-->
+<!--    <tbody>-->
+<!---->
+<!--    @if($users)-->
+<!---->
+<!--    @foreach($users as $user)-->
+<!---->
+<!--    <tr>-->
+<!--        <td>{{$user->id}}</td>-->
+<!--        <td><img height = "50" src="{{$user->photo? $user->photo->file:'http://placehold.it/400x400'}}" alt=""></td>-->
+<!--        <td><a href="{{route('users.edit', $user->id)}}">{{$user->name}}</a></td>-->
+<!--        <td>{{$user->email}}</td>-->
+<!--        <td>{{$user->role->name}}</td>-->
+<!--        <td>{{$user->is_active ? 'Active' : 'Inactive'}}</td>-->
+<!--        <td>{{$user->created_at->diffForHumans()}}</td>-->
+<!--        <td>{{$user->updated_at->diffForHumans()}}</td>-->
+<!--    </tr>-->
+<!---->
+<!--    @endforeach-->
+<!---->
+<!--    @endif-->
+<!--    </tbody>-->
+<!--</table>-->
+<!---->
+<!--@stop-->
