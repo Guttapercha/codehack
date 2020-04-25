9becbe514caa9cbf03f777ecc9968453ec037e14
Evgeniya Korchagina <evgeniya.korchagina@nbc.ca>
2020-04-25 00:18:09 -0400

    Posts display post' images

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -8,10 +8,10 @@
     <thead>
     <tr>
         <th>Id</th>
-        <th>User</th>
-        <th>Category</th>
         <th>Photo</th>
         <th>Title</th>
+        <th>User</th>
+        <th>Category</th>
         <th>Body</th>
         <th>Created</th>
         <th>Updated</th>
@@ -25,12 +25,11 @@
 
     <tr>
         <td>{{$post->id}}</td>
+        <td><img height = "50" src="{{$post->photo? $post->photo->file:'http://placehold.it/400x400'}}" alt=""></td>
+        <td><a href="{{route('posts.edit', $post->id)}}">{{$post->title}}</a></td>
         <td>{{$post->user->name}}</td>
         <td>{{$post->category_id != null ? $post->category_id: ''}}</td>
-        <td>{{$post->photo_id}}</td>
-        <td>{{$post->title}}</td>
         <td>{{$post->body}}</td>
-
         <td>{{$post->created_at->diffForHumans()}}</td>
         <td>{{$post->updated_at->diffForHumans()}}</td>
     </tr>
